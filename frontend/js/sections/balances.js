94e13643cd270f0222dcd3a7c3c6f247e552a672
bread <hi@claire.zone>
2022-10-07 02:29:57 -0500

    section empty state

--- frontend/js/sections/balances.js
+++ frontend/js/sections/balances.js
@@ -4,4 +4,4 @@
 const balanceRenderer = (appData, balance) => appData && appData.currency && appData.cap ? `
     <p>
         Current balance: <span class="balance-currency">${currencySign(appData.currency)}</span><span class="balance-value">${balance ? balance : 0.00}</span><span class="balance-out-of">/</span><span class="balance-currency">${currencySign(appData.currency)}</span><span class="balance-value">${appData.cap ? appData.cap : 0.00}</span>
-    </p>` : 'Counting beans&hellip;'
+    </p>` : '<div class="empty">Counting beans&hellip;</div>'
