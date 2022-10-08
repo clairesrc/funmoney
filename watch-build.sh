f180fdd126e8a958f99f4ab51dfd06e14f33ef9f
bread <hi@claire.zone>
2022-10-08 01:06:49 -0500

    remove outdated todo

--- watch-build.sh
+++ watch-build.sh
@@ -9,7 +9,6 @@ daemon() {
         if [[ $chsum1 != $chsum2 ]] ; then           
             if [ -n "$chsum1" ]; then
                 ENV=dev ./build-and-run.sh
-                # @TODO: live-reload: set env="DEV" env var here and have `build-and-run.sh` pass it down to `docker-compose up`. when that var is set to DEV in frontend container, the build script in frontend then appends some javascript that polls for changes in /app endpoint in the backend container. the baclend endpoint just needs an extra property added such as timestamp of server startup
             fi
             chsum1=$chsum2
         fi
